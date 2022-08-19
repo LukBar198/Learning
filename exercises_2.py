bfb55b91dd06acf53b1bdbe421ce3fdb1dc13256
Lukasz <lukasz.baran191@gmail.com>
2022-08-19 22:38:45 +0200

    Start learning Playtime with numbers.py

---
+++ exercises_2.py
@@ -0,0 +1,13 @@
+
+# Exercise 1 - basic number operations
+
+print(4+4)
+print((10-2))
+print(16/2)
+print(4*2)
+print(2**3)
+
+# Exercise 2
+
+favourite_number = 7
+print(f"\n\tMy favourite number is: {favourite_number}!")
