a35ff36d76d39210da4e99d112c8ea5cdf924836
Lukasz <lukasz.baran191@gmail.com>
2022-08-19 21:22:14 +0200

    NOw with new rows!

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 first_name = "Lukasz"
 last_name = "Baran"
 full_name1 = first_name + " " + last_name
-print("full_name1: ")
+print("\nfull_name1: ")
 print(full_name1)
 
 full_name2 = f"{first_name} {last_name}"
-print("full_name2: ")
+print("\nfull_name2: ")
 print(full_name2)
+
+print(f"\nHello, {full_name2.title()}")
