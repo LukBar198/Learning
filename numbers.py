185154ebd15503c0681dc6b7b3f9d6d8841eff46
Lukasz <lukasz.baran191@gmail.com>
2022-08-19 22:32:09 +0200

    Start learning Now with new rows! Added strip exercises_1 - finished! Playtime with numbers.py

---
+++ numbers.py
@@ -0,0 +1,18 @@
+
+# Different numbers notation
+
+universe_age1 = 14000000000
+print(universe_age1)
+universe_age2 = 14_000_000_000
+print(universe_age2)
+
+# Multiple variables initialization
+
+x, y, z = 0, 1, 2
+print(x, y, z)
+
+# Constant variables
+# Python don't special type for constant variables
+# We use CAPITAL LETTERS
+
+MAX_CONNECTIONS = 5000
