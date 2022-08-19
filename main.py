27e388ae24d47348f1317d49569fc72710b7854a
Lukasz <lukasz.baran191@gmail.com>
2022-08-19 21:12:21 +0200

    First step

---
+++ main.py
@@ -0,0 +1,9 @@
+first_name = "Lukasz"
+last_name = "Baran"
+full_name1 = first_name + " " + last_name
+print("full_name1: ")
+print(full_name1)
+
+full_name2 = f"{first_name} {last_name}"
+print("full_name2: ")
+print(full_name2)
