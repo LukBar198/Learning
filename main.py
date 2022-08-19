cda880d375d73b5f0bfa0adfbe83753f363f9210
Lukasz <lukasz.baran191@gmail.com>
2022-08-19 21:33:43 +0200

    Start learning Now with new rows!

--- main.py
+++ main.py
@@ -9,3 +9,12 @@ print("\nfull_name2: ")
 print(full_name2)
 
 print(f"\nHello, {full_name2.title()}")
+
+message = f"\nHow You doin' {full_name2}?"
+print(message)
+
+test = "  hhjjdd  "
+print(test)
+test = test.lstrip()
+test = test.rstrip()
+print(test)
