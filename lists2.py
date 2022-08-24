b7b5221f2a716510fe372ae5ccd22ae7704a4268
Lukasz <lukasz.baran191@gmail.com>
2022-08-24 22:01:09 +0200

    Start learning lists.py

---
+++ lists2.py
@@ -0,0 +1,11 @@
+
+magicians = ['zenek', 'martin', 'judasz']
+
+for magician in magicians:
+    print(f"\t{magician.title()} to byla wspaniala sztuczka!")
+    print(f"\tNie moge sie doczekac twojej nastepnej sztuczki, {magician.title()}.\n")
+
+print("Dziekuje wszystkim!")
+
+for value in range(1, 9):
+    print(value)
