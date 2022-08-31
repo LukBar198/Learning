0ba6291277aa419559fb2b27224150603de7b9fc
Lukasz <lukasz.baran191@gmail.com>
2022-08-31 23:09:21 +0200

    Start learning lists.py

---
+++ exercise_6.py
@@ -0,0 +1,17 @@
+
+for n in range(1, 21):
+    print(n)
+
+myl = list(range(1, 1000001))
+
+print(min(myl))
+print(max(myl))
+print(sum(myl))
+
+niep = list(range(1, 21, 2))
+for niepa in niep:
+    print(niepa)
+
+sq = [sqq**3 for sqq in range(3, 31)]
+for sqqq in sq:
+    print(sqqq)
--- lists2.py
+++ lists2.py
@@ -7,5 +7,22 @@ for magician in magicians:
 
 print("Dziekuje wszystkim!")
 
-for value in range(1, 9):
-    print(value)
+#for value in range(1, 9):
+   # print(value)
+
+numbers =list(range(1,9))
+print(numbers)
+
+parzyste = list(range(2, 50, 2))
+print(parzyste)
+
+squares = []
+
+for value in range(1,11):
+    square = value**2
+    squares.append(square)
+
+print(squares)
+print(min(squares))
+print(max(squares))
+print(sum(squares))
